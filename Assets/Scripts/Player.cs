673fed4c49de66b14c075d7c37e6d338a1c06638
SuSeMii <tkdals6525@gmail.com>
2020-09-30 03:00:21 +0900

    first commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    bool isCanMove = false;
+    public float speed = 6.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isCanMove == true){
+            float h = Input.GetAxisRaw("Horizontal");
+            //
+            float v = Input.GetAxisRaw("Vertical");
+            //
+            Vector3 curPos = transform.position;
+            Vector3 nextPos = new Vector3(h,v,0) * speed * Time.deltaTime;
+
+            transform.position = curPos + nextPos;
+        }
+    }
+    private void OnTriggerEnter(Collider other) {
+        
+    }
+}
